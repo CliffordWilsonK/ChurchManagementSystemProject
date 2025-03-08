0fc1b56a3e9ca765d0c40882f3056be540f92cb9
[Clifford Wilson] <[cliffordwilsonk@gmail.com]>
2025-03-08 15:33:28 +0000

    Made the ChurchProject model

--- ChurchManagementSystem/church_management/models.py
+++ ChurchManagementSystem/church_management/models.py
@@ -12,3 +12,11 @@ class Financial_Transactions(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     date = models.DateField(auto_now_add=True)
 
+
+class ChurchProjects(models.Model):
+    name = models.CharField(max_length=10)
+    description = models.TextField()
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    collected_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
