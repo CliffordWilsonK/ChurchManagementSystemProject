d6cad2e3489198116483550ae1964257867cce95
[Clifford Wilson] <[cliffordwilsonk@gmail.com]>
2025-03-08 18:38:37 +0000

    Updated the views and templates

--- ChurchManagementSystem/church_management/views.py
+++ ChurchManagementSystem/church_management/views.py
@@ -4,9 +4,9 @@ from django.db import models
 
 # Create your views here.
 def dashboard(request):
-    total_tithes = Financial_Transaction.objects.filter(category='tithe').aggregate(total=models.Sum('amount'))['amount_sum'] or 0
-    total_offering = Financial_Transaction.objects.filter(category='offering').aggregate(total=models.Sum('amount'))['amount_sum'] or 0
-    total_seed = Financial_Transaction.objects.filter(category='offering').aggregate(total=models.Sum('amount'))['amount_sum'] or 0
+    total_tithes = Financial_Transaction.objects.filter(category='tithe').aggregate(total=models.Sum('amount'))['total'] or 0
+    total_offerings = Financial_Transaction.objects.filter(category='offering').aggregate(total=models.Sum('amount'))['total'] or 0
+    total_seed = Financial_Transaction.objects.filter(category='seed').aggregate(total=models.Sum('amount'))['total'] or 0
 
     projects = ChurchProject.objects.all()
 
@@ -14,9 +14,9 @@ def dashboard(request):
 
     recent_transactions = Financial_Transaction.objects.order_by('-date')[:10]
 
-    return render(request, 'church_mangement/dashboard.html', {
+    return render(request, 'church_management/dashboard.html', {
         'total_tithes': total_tithes,
-        'total_offering': total_offering,
+        'total_offerings': total_offerings,
         'total_seed': total_seed,
         'projects': projects,
         'total_members': total_members,
