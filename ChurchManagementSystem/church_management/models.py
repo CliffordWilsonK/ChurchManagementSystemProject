26542b6fab56444eb2e8c247f48c2883d7eb3cbf
[Clifford Wilson] <[cliffordwilsonk@gmail.com]>
2025-03-08 15:23:57 +0000

    Made the Financial Transaction models

--- ChurchManagementSystem/church_management/models.py
+++ ChurchManagementSystem/church_management/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class church_finance(models.Model):
-    tithes = models.IntegerField()
-    offerings = models.IntegerField()
-    seeds = models.IntegerField()
+class Financial_Transactions(models.Model):
+    CATEGORY_CHOICES = [
+        ('tithe', 'Tithe'),
+        ('offering', 'Offering'),
+        ('seed', 'Seed'),
+    ]
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    date = models.DateField(auto_now_add=True)
+
