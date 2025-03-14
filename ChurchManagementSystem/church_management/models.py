76a49c7f26c5ef3c4053d832eef7c843ac4cbda0
[Clifford Wilson] <[cliffordwilsonk@gmail.com]>
2025-03-14 11:40:14 +0000

    Changed the database

--- ChurchManagementSystem/church_management/models.py
+++ ChurchManagementSystem/church_management/models.py
@@ -8,7 +8,7 @@ class Financial_Transaction(models.Model):
         ('seed', 'Seed'),
     ]
 
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=100, decimal_places=2)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     date = models.DateField(auto_now_add=True)
 
@@ -16,8 +16,8 @@ class Financial_Transaction(models.Model):
 class ChurchProject(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    collected_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    target_amount = models.DecimalField(max_digits=100, decimal_places=2)
+    collected_amount = models.DecimalField(max_digits=100, decimal_places=2, null=True, blank=True)
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
 
@@ -43,7 +43,7 @@ class ChurchMember(models.Model):
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
+    phone_number = models.CharField(max_length=15, blank=True null=True)
     email= models.EmailField(blank=True, null=True)
     address = models.CharField(max_length=100)
     date_of_birth = models.DateField()
